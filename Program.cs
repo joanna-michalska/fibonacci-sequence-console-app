4c6ae17c363446fa14fdfb87b37fe6d4d6c17054
Joanna Michalska <asiamichalska9@gmail.com>
2022-12-12 12:09:34 +0100

    Adding Fibonacci console app solution

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of elementes in Fibonacci sequence: ");
+            int.TryParse(Console.ReadLine(), out int fibonacciSequenceAmount);
+
+            int a = 0;
+            int b = 1;
+            int sum;
+
+            for (int i = 0; i < fibonacciSequenceAmount; i++)
+            {
+                Console.WriteLine(a);
+                sum = a + b;
+                a = b;
+                b = sum;
+            }
+        }
+    }
+}
